b5d6d3f30e2a792cb8eb5fc222bd22e92f54ecd1
Ahmet Sari <106359636+eeeng@users.noreply.github.com>
2022-07-01 16:10:32 +0300

    Update optimization-solver.py

--- optimization-solver.py
+++ optimization-solver.py
@@ -1,5 +1,5 @@
 #i
-var='k' or 'k1'
+ #var='k' or'k1'
 
 N=[1,2,3]
 
@@ -8,11 +8,7 @@ cost = lambda C,F: 10000//C * 2 * F * 100
 #ii 
 def transCost(C,F):
     cost(C, F)
-   
-    def transCost2(C,F):
-        print(f"{C} {F} \ncost={cost}")
-        return cost
-    
+    print(f"{C} {F} \ncost={cost}")   
     return cost
         
 print("cost = 10000 / C * 2 * F * 100")
@@ -21,34 +17,36 @@ while(1):
     for k in N:    
         if N[0] == 1:
             cost0 = cost(10000,4)
-            print("", k,"\t \t", cost0, "\n",
+            print("", N[0],"\t \t", cost0, "\n",
                   "{} {}".format(10000, 4))
           
                
         if N[1] == 2:
-            k+=1
+                # k+=1
             cost1 = cost(2000, 3)
-            print("", k, "\t \t", "{} {}".format(2000, 3), "\n",
+            print("", N[1], "\t \t", "{} {}".format(2000, 3), "\n",
                   "{} {}".format(1000,8), "",cost1, "\n",
                   "{} {}".format(2000,3))
             
          
         if N[2] == 3:
-            k+=1
+                # k+=1
             cost2 = cost(100, 6)
-            print("", k , "\t \t", "{} {}".format(100, 6), "\n",
+            print("", N[2] , "\t \t", "{} {}".format(100, 6), "\n",
                   "{} {}".format(50, 4), "\t", cost2, "\n", 
                   "{} {}".format(100, 8) , "\n",
                   "{} {}".format(100, 6))
+            continue
 #iii
-            input("N: ")
-            if N[0] == 1:                
-                cost0 = cost(10000,4)
+                                       
+            #input("N: ")
+            if N[0]==1:                
                 print("", N[0],"\t \t", cost0, "\n",
                       "{} {}".format(10000, 4))
-            input("N: ")        
-            if N[1] == 2:                
-                print("", N[1], "\t \t", "{} {}".format(2000, 3), "\n",
+                
+                
+            else:    
+                print("", N[0], "\t \t", "{} {}".format(2000, 3), "\n",
                       "{} {}".format(1000,8), "",cost1, "\n",
                       "{} {}".format(2000,3))
 
